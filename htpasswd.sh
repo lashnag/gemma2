5cd58b652e84663f7ee54890ce16a1216d717786
a.lashnev <lexey2006.90@mail.ru>
2025-02-24 19:46:06 +0300

    Fix

--- htpasswd.sh
+++ htpasswd.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
+
 echo "${GEMMA2_USERNAME}:$(openssl passwd -apr1 ${GEMMA2_PASSWORD})" > /etc/nginx/.htpasswd
-exec nginx -g 'daemon off;'
+
+cat <<EOF > /etc/nginx/nginx.conf
+server {
+    listen ${GEMMA2_PORT};
+
+    location / {
+        proxy_pass http://ollama:11434;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+
+        # Настройка базовой авторизации
+        auth_basic "Restricted Area";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+    }
+}
+EOF
+
+nginx -g 'daemon off;'
