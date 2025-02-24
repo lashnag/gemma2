dd4e8e7e0295c0b5acd240a0a5e82d3b65748a74
a.lashnev <lexey2006.90@mail.ru>
2025-02-24 18:39:25 +0300

    Fix nginx run command

--- htpasswd.sh
+++ htpasswd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "${OLLAMA_USERNAME}:$(openssl passwd -apr1 ${OLLAMA_PASSWORD})" > /etc/nginx/.htpasswd
+echo "${GEMMA2_USERNAME}:$(openssl passwd -apr1 ${GEMMA2_PASSWORD})" > /etc/nginx/.htpasswd
 exec nginx -g 'daemon off;'
